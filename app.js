334382068520e9a2e3b2187f362d647f6a9cf7d7
Barry Tabios <b_tabios@yahoo.com>
2017-03-27 11:26:22 +1100

    commit2

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ $setColorBtn.on('click', function(){
   brush.style.backgroundColor = input;
 
 });
+
+$inputColor = $('.input');
+$inputColor.on('keypress', function(){
+  input = document.querySelector('.input').value;
+  brush = document.querySelector('.brush');
+  brush.style.backgroundColor = input;
+}); 
