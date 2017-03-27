80c9665b4c2a6270fb293ff10277bfa19b1e6e37
Julian Chan <julian.chan@mail.com>
2017-03-27 11:12:49 +1100

    moved script tags to head; wrapped js code in jquery document ready; 20 "square" divs appended to body

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-console.log('main.js connected');
+$(function() {
+  console.log('main.js connected');
 
-var setBrushBoxColor = function() {
-  var color = $('input').val();
-  $('.brush-box').css('background', color);
-}
+  var setBrushBoxColor = function() {
+    var color = $('input').val();
+    $('.brush-box').css('background', color);
+  };
 
-$('button').click(function() {
-  setBrushBoxColor();
-});
-
-$('input').keydown(function(e) {
-  if (e.which === 13) {
+  $('button').click(function() {
     setBrushBoxColor();
+  });
+
+  $('input').keydown(function(e) {
+    if (e.which === 13) {
+      setBrushBoxColor();
+    }
+  });
+
+  var $body = $('body');
+
+  for (var i = 0; i < 20; i++) {
+    $body.append($('<div>').addClass('square'));
   }
 });
