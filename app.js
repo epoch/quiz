471f934d4d7e5bbc8417d3840c391bbfb9918cf2
Zach Chrystall <z.chrystall@gmail.com>
2017-03-27 11:34:21 +1100

    modified app.j to update color on enter submit

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 console.log('i want to paint the world');
 
 
-var $brushBox = $('.brush')
-var $setColor = $('.set-color')
+var $brushBox = $('.brush');
+var $setColor = $('.set-color');
+var $brushColorInput = $('.color');
+
+
+var changeBrushBox = function(event) {
+  $brushBox.css('background-color', $brushColorInput.val());
+}
 
 $setColor.on('click', function(event) {
   event.preventDefault();
-  var $brushColorInput = $('.color')
-  $brushBox.css('background-color', $brushColorInput.val())
+  changeBrushBox();
+});
+
+$brushColorInput.keypress( function(event) {
+  if (event.which == 13) {
+    event.preventDefault();
+    changeBrushBox();
+  }
 });
