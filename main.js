68914e50162ccb0477dc4032a8f38f347e936b70
Julian Chan <julian.chan@mail.com>
2017-03-27 11:41:21 +1100

    added swatches for 3 most recent colors; clicking on a swatch sets brush box color to swatch color

--- main.js
+++ main.js
@@ -2,15 +2,37 @@ $(function() {
   console.log('main.js connected');
 
   var $body = $('body');
-  var $brushBox = $('.brush-box')
+  var $brushBox = $('.brush-box');
   var $colorInput = $('input');
+  var $swatches = $('.swatch');
+  var recentColors = [];
 
   var setColorOf = function($element, color) {
     $element.css('background', color);
   };
 
   var setBrushBoxColor = function() {
-    setColorOf($brushBox, $colorInput.val());
+    var color = $colorInput.val();
+
+    setColorOf($brushBox, color);
+
+    if (recentColors.includes(color)) {
+      recentColors.splice(recentColors.indexOf(color), 1);
+    }
+
+    recentColors.unshift(color);
+
+    if (recentColors.length > 3) {
+      recentColors.pop();
+    }
+
+    updateSwatches();
+  };
+
+  var updateSwatches = function() {
+    for (var i = 0; i < recentColors.length; i++) {
+      $swatches.eq(i).css('background', recentColors[i]);
+    }
   };
 
   $('button').click(function() {
@@ -28,6 +50,10 @@ $(function() {
   }
 
   $body.on('mouseover', '.square', function(e) {
-    setColorOf($(e.target), $colorInput.val());
+    setColorOf($(e.target), $brushBox.css('background'));
+  });
+
+  $swatches.click(function(e) {
+    setColorOf($brushBox, $(e.target).css('background'));
   });
 });
