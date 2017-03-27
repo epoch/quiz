2e539f67cbf839a35d236df4cf9bba1e3c101be8
Barry Tabios <b_tabios@yahoo.com>
2017-03-27 11:45:37 +1100

    commit3

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ $setColorBtn.on('click', function(){
   input = document.querySelector('.input').value;
   brush = document.querySelector('.brush');
   brush.style.backgroundColor = input;
+  //
+  // var newElement = $('<div>').className="square";
+  // $('body').append(newElement);
+
+  for (i=0; i<20; i++){
+    var newElement = document.createElement('div');
+    newElement.setAttribute('class', 'square');
+    document.querySelector('body').appendChild(newElement);
+  };
 
 });
 
@@ -13,4 +22,13 @@ $inputColor.on('keypress', function(){
   input = document.querySelector('.input').value;
   brush = document.querySelector('.brush');
   brush.style.backgroundColor = input;
-}); 
+});
+
+
+// title = result[i]['Title'];
+//     newH2 = document.createElement('H2');
+//     newH2.setAttribute("id", i );
+//     document.querySelector('body').appendChild(newH2);
+//     // document.querySelector(.i).innerHTML = title;
+//     document.getElementById(i).innerHTML = title;
+//     console.log(title);
