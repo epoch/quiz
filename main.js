9354ee8922cd07354c3a007d274fcfd85d049c6c
Gerard Ho <gerard.ho@live.com.au>
2017-03-27 12:54:07 +1100

    commit 7

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 console.log('hello working')
+var arrayColors = [];
 
 $('button').on('click',function(event){
   event.preventDefault();
   var $color = $('input').val();
   $('button').css('background-color',$color);
   $('input').css('background-color',$color);
+  colorSwatch($color);
 });
 
 $('input').keypress(function(event){
@@ -12,6 +14,7 @@ $('input').keypress(function(event){
     var $color = $('input').val();
     $('button').css('background-color',$color);
     $('input').css('background-color',$color);
+    colorSwatch($color);
   }else{
     return;
   }
@@ -19,16 +22,44 @@ $('input').keypress(function(event){
 
 function squareDivs(num){
   var $div = $('<div>');
-  return $div.addClass("square").text(num);
+  return $div.addClass("square");
 }
-function makeDiv(){
-  for(var i=0; i<=20;i++){
+function makeDiv(num){
+  for(var i=0; i<=num;i++){
     $('body').append(squareDivs(i));
   }
 }
-makeDiv();
+
+function colorDivs(){
+  // debugger;
+  $('.color-swatch-1').css('background-color',arrayColors[0]);
+  $('.color-swatch-2').css('background-color',arrayColors[1]);
+  $('.color-swatch-3').css('background-color',arrayColors[2]);
+}
+
+function colorSwatch(color){
+  // debugger;
+  if (arrayColors.length === 3){
+    arrayColors.shift();
+    arrayColors.push(color);
+    colorDivs();
+  }else{
+    arrayColors.push(color);
+    colorDivs();
+  }
+}
+
+
+makeDiv(2000);
 
 $('div.square').hover(function(event){
   var $color = $('input').val()
   event.target.style["background-color"] = $color;
 });
+
+$('.color-swatch').on('click', function(event){
+  var $color = $(event.target).css('background-color');
+  $('input').css('background-color',$color);
+  $('input').val($color);
+  $('button').css('background-color',$color);
+})
