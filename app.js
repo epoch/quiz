8934d9c1f45de631baa011ee3b307307740187f1
SamWij <sam.wijesinha@gmail.com>
2017-03-27 17:39:06 +1100

    commit 2 - enter button

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $('.colourChange').click(function(){
+  changeBrushColour()
+
+});
+
+$('.colourType' ).keypress(function() {
+  changeBrushColour()
+});
+
+var changeBrushColour = function(){
   var colour = $('.colourType').val();
   $(".brush").css('background-color', colour)
 
-});
+};
