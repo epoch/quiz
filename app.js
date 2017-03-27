80cab3323fd4238ebc1b0aad5a5a31b5b47056b3
SamWij <sam.wijesinha@gmail.com>
2017-03-27 18:54:46 +1100

    commit 7

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+var swatch = []
+
 $('.colourChange').click(function(){
+   event.preventDefault();
   changeBrushColour()
 
 });
 
-$('.colourType' ).keypress(function() {
-  changeBrushColour()
+$('.colourType' ).keydown(function(e) {
+  if (e.keyCode == 13) {
+    changeBrushColour()
+      addColor(colour)
+  }
 });
+
+
 
 var changeBrushColour = function(){
   var colour = $('.colourType').val();
+  console.log(colour)
   $(".brush").css('background-color', colour)
+  addColor(colour)
 
 };
 
@@ -20,3 +30,24 @@ for (var i = 0; i < 20; i++) {
 $( ".square" ).on( "click", function() {
   $(this).css('background-color', 'green');
 });
+
+
+var addColor = function(colour){
+  if (swatch.length === 3) {
+    swatch.shift()
+    swatch.push(colour)
+    swatches()
+  } else {
+    swatch.push(colour)
+    swatches()
+  }
+
+}
+
+var swatches = function() {
+  for (var i = 0; i < swatch.length; i++) {
+    var swatchDiv = $('<div>').addClass('swatch').attr('id', i).appendTo('body')
+
+  }
+    swatchDiv.css('background-color', swatch[i]).html(swatch[i])
+};
