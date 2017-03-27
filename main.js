2be46f20afe454888fe6d793028eeba30e83f45d
Kerry <kerrysng@gmail.com>
2017-03-27 11:32:29 +1100

    commit 1

---
+++ main.js
@@ -0,0 +1,10 @@
+var change = document.getElementById('button')
+
+var colorInput = document.getElementById('color').value;
+
+change.addEventListener('click', function() {
+console.log('hello');
+
+  // colorResult.style.color = colorInput;
+
+})
