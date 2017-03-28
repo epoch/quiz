8805e3dec254c9ba263757b71532047497add9b6
will <mail@wilmaguire.com>
2017-03-28 23:27:43 +1100

    commit3

--- javascript.js
+++ javascript.js
@@ -11,5 +11,16 @@ $('#color').keypress(function(e){
   $color_choice = $('#color').val();
   if (e.which === 13) {
     $('.brush_box').css("background-color", $color_choice)
-  }  
+  }
+});
+
+
+
+
+$('.body').ready(function () {
+    var $newDiv = $('<div class="square"/>');
+    for (var i = 0; i <= 20; i++) {
+        $newDiv = $('<div class="ball" />').text(i);
+        $('body').append($newDiv);
+    }
 });
